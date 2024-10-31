ed84e83f6303696e0ee426a371e1792dd36ce9e2
JessAngiolini <jlangiolini@gmail.com>
2024-10-31 08:20:42 -0300

    actualizando carpeta backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import userRoutes from "./routes/usersRoutes";
+import panelProductRoutes from "./routes/panelProductsRoutes";
+import productsRoutes from "./routes/publicProductsRoutes";
+import orderRoutes from "./routes/orderRoutes";
 
 const app = express();
 
@@ -10,6 +13,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(userRoutes);
+app.use(panelProductRoutes);
+app.use(productsRoutes);
+app.use(orderRoutes);
 
 export default app;
-
--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -1,9 +1,8 @@
 import {Router} from "express";
-import {getUsers, saveRegister, LoginCtrl} from '../controllers/usersControllers';
+import { saveRegister, LoginCtrl, } from '../controllers/usersControllers';
 
 const router = Router();
 router.post("/register", saveRegister)
-router.get('/users',getUsers)
 router.post('/login', LoginCtrl) 
 
 export default router; 
